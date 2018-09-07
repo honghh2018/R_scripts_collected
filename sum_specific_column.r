827edaf7d059d05da5cf97fd0d74e7bf0e3a2aa4
Future <41668708+honghh2018@users.noreply.github.com>
2018-09-07 16:23:19 +0800

    Update sum_specific_column.r

--- sum_specific_column.r
+++ sum_specific_column.r
@@ -5,8 +5,11 @@ infile2<-read.table(args[2],header=T,row.name=1)
 result1<-sum(infile1$T3)
 print (result1)    #bracket must be given
 result2<-sum(infile2$T3)
-print (result2)   #bracket must be given,auto newline
-
+print (result2)   #bracket must be given,auto newline，but cat not auto newline,you should add "\n"
+cat("this is a test\n")
+para<-length(args)
+cat("para\n");  #output para and "\n"
+cat(para)  #no auto newline,output value for para
 #comment:double quotation represent a string not a variable value
 
 #USAGE:
