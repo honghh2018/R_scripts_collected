d42a3b0f59876c2b027f751877dd0e7863878072
Future <41668708+honghh2018@users.noreply.github.com>
2018-09-07 16:13:35 +0800

    Update sum_specific_column.r

--- sum_specific_column.r
+++ sum_specific_column.r
@@ -3,11 +3,11 @@ args<-commandArgs(T)
 infile1<-read.table(args[1],header=T,row.name=1)
 infile2<-read.table(args[2],header=T,row.name=1)
 result1<-sum(infile1$T3)
-print result1
+print (result1)    #bracket must be given
 result2<-sum(infile2$T3)
-print result2
+print (result2)   #bracket must be given,auto newline
 
-
+#comment:double quotation represent a string not a variable value
 
 #USAGE:
 #Rscript sum_specific_column.r a.list b.list
