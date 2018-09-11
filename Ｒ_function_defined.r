a4de3996c2ff2f3948b69709d54fea30e0595f68
Future <41668708+honghh2018@users.noreply.github.com>
2018-09-11 13:19:26 +0800

    Create Ｒ_function_defined.r

---
+++ Ｒ_function_defined.r
@@ -0,0 +1,16 @@
+/usr/bin/Rscript
+sum<-function(x){
+  mysum<-0  #the activating effect area
+  for(i in 1:length(x)){
+    mysum<-mysum+x[i]   #accumulating the values
+  }
+  print(mysum)
+}
+sum(c(1,2,3,6)) #invoking the function sum
+
+
+
+
+
+
+
