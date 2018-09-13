13a5dd2b3fcc4150ee47e749babe032a3bbeba23
Future <41668708+honghh2018@users.noreply.github.com>
2018-09-13 18:00:36 +0800

    Update output_result_to_file.R

--- output_result_to_file.R
+++ output_result_to_file.R
@@ -9,8 +9,17 @@ sink()
 [1]5 10
 
 data<-read.table("test_test.txt",header=T,row.names=1)  #same as perl input handle
-data<-data+10
+data<-data+10　　＃every element add ten via this scripts
 write.table(data,file="this_is_a_test.txt",row.names=F,col.names=F) #same as perl output handle
+
+sometime,you have lot of things  would interrupt youself that you should let it go by no mercy,because those things inevitable keep your live stuck.
+so put them behind you when you move on.
+this was not a scripts,just wrote something i want to say on trivial　daily live day by day.
+in any case,your copy this scripts for your work,just used # ignored it.good luck my friends
+
+
+
+
 
 
 
