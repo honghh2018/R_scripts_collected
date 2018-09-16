d69a8e27e20322ac174a884e66722ed5f9e6f5e1
Alsace <41668708+honghh2018@users.noreply.github.com>
2018-09-16 00:11:40 -0600

    Update augment_specific_column.R

--- augment_specific_column.R
+++ augment_specific_column.R
@@ -15,3 +15,31 @@ for (i in 2:nrow(myres)){
 }
 
 write.table(myres,file="result.txt",append=FALSE,quote=FALSE,col.names=TRUE,sep="\t")
+
+##########################################
+#read file and tranfer to matrix
+mydata<-as.matrix(read.table("test.txt",header=F,sep="\t")) #read in T01, T02 and so on matrix
+myhead_id<-mydata[1,] #get column name
+mydata<-mydata[-1,]  #delete first row
+#myres<-matrix(NA,nrow=nrow(mydata),ncol=ncol(mydata),sep="\t")
+b<-paste(myhead_id[2],myhead_id[3],sep="_")
+myres<-matrix(NA,nrow=nrow(mydata),ncol=4)
+#colnames(c(myhead_id[1],paste(myhead_id[2],myhead_id[3],sep="_"),paste(myhead_id[4],myhead_id[5],sep="_"),paste(myhead_id[6],myhead_id[7],sep="_")))
+
+for (i in 1:nrow(myres)){
+ # colnames(myres[1,])<-c(myhead_id[1],paste(myhead_id[2],myhead_id[3],sep="_"),paste(myhead_id[4],myhead_id[5],sep="_"),paste(myhead_id[6],myhead_id[7],sep="_"))
+    #get T01_T02 format
+  colnames(myres[1,],do.NULL = TRUE,prefix="col")  #the right scripts 
+  colnames(myres)<-c(myhead_id[1],paste(myhead_id[2],myhead_id[3],sep="_"),paste(myhead_id[4],myhead_id[5],sep="_"),paste(myhead_id[6],myhead_id[7],sep="_"))
+   myres[i,1]=mydata[i,1] #get gene_id
+  myres[i,2]=as.numeric(mydata[i,2])+as.numeric(mydata[i,3])    #sum of two column
+  myres[i,3]=as.numeric(mydata[i,4])+as.numeric(mydata[i,5])
+  myres[i,4]=as.numeric(mydata[i,6])+as.numeric(mydata[i,7])
+}
+myres
+write.table(myres,file="result.txt",append=FALSE,quote=FALSE,col.names=TRUE,sep="\t")
+#quota=TRUE was add "" for output file.
+
+
+
+
