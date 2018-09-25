2054eaf1e76ed5041f152f6c43c357fac7188dc1
Alsace <41668708+honghh2018@users.noreply.github.com>
2018-09-25 13:42:41 +0800

    Update transposition.R

--- transposition.R
+++ transposition.R
@@ -11,4 +11,31 @@ in_file<-read.delim(opt$infile,sep="\t",header=FALSE,row.names=1) #if it have he
 result<-t(in_file)
 write.table(result,file=opt$output,sep="\t",quote=FALSE,row.names=FALSE)
 
+＃＃advanced scripts
+library(getopt)
+
+spec=matrix(c(
+  'infile', 'i', 1, "character",
+  'output', 'o', 1, "character",
+  'help', 'h', 0, "logical"
+),byrow=TRUE,ncol=4);
+opt = getopt(spec);
+
+print_usage<-function(spec=NULL){  #formal parameter
+	cat(getopt(spec,usage=TRUE));
+	cat("Usage example:\n");
+	cat("
+		Usage example:
+		Rscripts transposition.R -i <infile> -o <outfile>
+	\n")
+	q(status=1);
+}
+##start run the scripts##
+if(is.null(opt$infile) ||is.null(opt$output)){print_usage(spec)}  #actual parameters
+
+in_file<-read.delim(opt$infile,sep="\t",header=FALSE,row.names=1)
+result<-t(in_file)
+write.table(result,file=opt$output,sep="\t",quote=FALSE,row.names=FALSE)
+
+
 
